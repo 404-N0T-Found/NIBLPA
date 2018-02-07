e79a3f92ce68865a8403b4e499221323b8c9b2ca
Meysam Azad <MeysamAzad81@yahoo.com>
2018-02-07 19:39:42 +0330

    added function 'initialize' to read the number of nodes and their corresponding edges

--- src/network.cpp
+++ src/network.cpp
@@ -1,63 +1,79 @@
 #include "network.h"
 
-network::network(string inputFile, string outputFile = "")
+Network::Network()
 {
-
+    numberOfNodes = 0;
 }
 
-void network::init()
+bool Network::initialize(string inputPath)
 {
-    cout << "Enter number of network nodes: ";
-    cin >> numberOfNodes;
-    testNetwork = new Node[numberOfNodes + 1];
-    cout << "Enter node labels each node is connected to; Zero if none.\n";
-    for (int i = 1; i <= numberOfNodes; i++)
-        testNetwork[i] = Node(i, numberOfNodes);
-    for (int i = 1; i <= numberOfNodes; i++)
+    try
     {
-        cout << i << ". ";
-        string temp;
-        if (i == 1)
-            getline(cin, temp);
-        getline(cin, temp);
-        string delimiter = " ";
-        size_t pos = 0;
-        while ((pos = temp.find(delimiter)) != string::npos)
+
+        QFile inputFile(QString::fromStdString(inputPath));
+
+        if (!inputFile.open(QFile::ReadOnly)) // try to open the file
+            return false;
+
+
+
+
+        // first loop to get the number of nodes
+        while (!inputFile.atEnd()) // read until EOF
         {
-            string tmp = temp.substr(0, pos);
-            try
-            {
-                int tmpNum = atoi(tmp.c_str());
-                if (tmpNum >= 1 && tmpNum <= numberOfNodes)
-                {
-                    testNetwork[i].addEdge(tmpNum);
-                    testNetwork[tmpNum].addEdge(i);
-                }
-            }
-            catch(exception e)
-            {
-                cout << e.what();
-            }
-            temp.erase(0, pos + delimiter.length());
+            QString line = inputFile.readLine(); // read line by line
+            line = line.replace('\n', "\0"); // remove the endl
+
+            QStringList list = line.split('\t'); // get two nodes
+
+            int v1 = list.at(0).toInt(),
+                    v2 = list.at(1).toInt();
+
+            int value = (v2 > v1 ? v2 : v1);
+
+            if (value > numberOfNodes)
+                numberOfNodes = value;
         }
-        delimiter = "\n";
-        if ((pos = temp.find(delimiter)) != string::npos);
+
+
+        // now get the edges
+        inputFile.seek(0); // put the cursor at the beginning of the file
+
+
+        // node labels start from zero so we should initializr from 1 to n
+        edges = new SparseMatrix<int>(numberOfNodes + 1);
+
+
+
+
+
+
+        // second loop to get the edges
+        while (!inputFile.atEnd()) // get number of nodes
         {
-            string tmp = temp.substr(0, pos);
-            try
-            {
-                int tmpNum = atoi(tmp.c_str());
-                if (tmpNum >= 1 && tmpNum <= numberOfNodes)
-                {
-                    testNetwork[i].addEdge(tmpNum);
-                    testNetwork[tmpNum].addEdge(i);
-                }
-            }
-            catch(exception e)
-            {
-                cout << e.what();
-            }
-            temp.erase(0, pos + delimiter.length());
+            QString line = inputFile.readLine();
+            line = line.replace('\n', "\0");
+
+
+            QStringList list = line.split('\t');
+
+
+            int v1 = list.at(0).toInt(),
+                    v2 = list.at(1).toInt();
+
+
+            // indirected graph; having edge in both direction
+            edges->set(v1, v2, 1);
+            edges->set(v2, v1, 1);
+
+
         }
+
+        // close the reading file
+        inputFile.close();
+    }
+    catch(exception e)
+    {
+        cout << e.what();
     }
 }
