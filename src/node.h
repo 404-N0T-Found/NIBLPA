5bdc69a517e80db0873e9fa297d88a8bb76ad2d1
Meysam Azad <MeysamAzad81@yahoo.com>
2018-02-07 19:44:08 +0330

    friend class Network, to edit kShell and degree

--- src/node.h
+++ src/node.h
@@ -7,6 +7,9 @@
 #include <string.h>
 #include <algorithm>
 
+
+#include "network.h"
+
 using namespace std;
 
 class Node
@@ -20,6 +23,8 @@ private:
 public:
     Node();
     Node(int label);
+
+    friend class Network;
 };
 
 
