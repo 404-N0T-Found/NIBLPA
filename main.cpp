df8295ab04a600735dca7813b8d357b8ee30527d
Meysam Azad <MeysamAzad81@yahoo.com>
2018-02-08 19:27:58 +0330

    THE END

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
-#include <iostream>
 #include <src/network.h>
 
-using namespace std;
 
-int main()
+
+int main(int argc, char *argv[])
 {
-    network t;
-    t.init();
-    cout << t;
+    Network network;
+
+    string outputPath = "../outputFile.txt";
+
+
+    if (argc == 2) // just input file passed as command line argument
+    {
+        network.initialize(argv[1]);
+    }
+    else if (argc == 3) // input and output
+    {
+        network.initialize(argv[1]);
+        outputPath = argv[2];
+    }
+    else // none
+    {
+        cout << "Usage: main.o <input file path>"
+                " [<output file path>]\n";
+        return 4;
+    }
+
+    // start of the NIBLPA
+    if (network.computeKShell())
+        if (network.computeNodeInfluence())
+            if (network.computeLabelInfluence())
+                if (network.computeNewLabels())
+                    network.writeResultsToFile(outputPath);
+    // end of the NIBLPA
+
     return 0;
 }
